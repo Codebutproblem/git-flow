b86fa53fbdd95385e88d9558dd17999073cee255
minh_vu0201 <127370973+Codebutproblem@users.noreply.github.com>
2024-07-15 23:14:15 +0700

    create random string function

--- main.js
+++ main.js
@@ -7,3 +7,19 @@ const countdown = setInterval(() => {
     const difference = newYear - now;
     timeLeft.innerHTML = `${Math.floor(difference / (1000 * 60 * 60 * 24))} days ${Math.floor((difference / (1000 * 60 * 60)) % 24)} hours ${Math.floor((difference / 1000 / 60) % 60)} minutes ${Math.floor((difference / 1000) % 60)} seconds`;
 }, 1000);
+
+
+// Random String Generator
+const randomButton = document.getElementById('random-button');
+const randomResult = document.getElementById('random-result');
+const randomLength = document.getElementById('random-length');
+randomButton.addEventListener('click', () => {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const length = randomLength.value;
+    let result = "";
+
+    for(let i = 0; i < length; i++){
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    randomResult.innerHTML = result;
+});
