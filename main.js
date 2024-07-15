a4baf178f9cf8ce725138620ed0445548a473563
minh_vu0201 <127370973+Codebutproblem@users.noreply.github.com>
2024-07-15 23:08:00 +0700

    initial

---
+++ main.js
@@ -0,0 +1,9 @@
+
+const timeLeft = document.getElementById('second-left');
+const countdown = setInterval(() => {
+    const now = new Date();
+    const currentYear = now.getFullYear();
+    const newYear = new Date(currentYear + 1, 0, 1);
+    const difference = newYear - now;
+    timeLeft.innerHTML = `${Math.floor(difference / (1000 * 60 * 60 * 24))} days ${Math.floor((difference / (1000 * 60 * 60)) % 24)} hours ${Math.floor((difference / 1000 / 60) % 60)} minutes ${Math.floor((difference / 1000) % 60)} seconds`;
+}, 1000);
